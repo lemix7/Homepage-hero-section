091dd99646798272491886c8eb43e1ee77cb0459
Ahmad <ahmedgeger@gmail.com>
2024-09-10 11:24:02 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./HomePage/*.{html,js}"],
+  theme: {
+   
+    extend: {
+      colors:{
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
